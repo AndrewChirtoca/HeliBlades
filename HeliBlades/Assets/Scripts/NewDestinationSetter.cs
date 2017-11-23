9a9c7709a5550cef33c5503c198bba61b87b6d54
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-23 03:03:41 +0200

    nav agent destination setter script that looks up the position from the specified Vector3Variable on demand

---
+++ HeliBlades/Assets/Scripts/NewDestinationSetter.cs
@@ -0,0 +1,49 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-23 02:47:32
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// NewDestinationSetter class.
+    /// </summary>
+    public class NewDestinationSetter : MonoBehaviour
+    {
+#region Public serialized variables
+        public NavMeshAgent agent;
+        public Vector3Variable destinationLookup;
+#endregion
+
+
+
+#region Private variables
+#endregion
+
+
+
+#region Public methods and properties
+        public void LookupDestination()
+        {
+            agent.SetDestination(destinationLookup.value);
+        }
+#endregion
+
+
+
+#region Monobehavior methods
+#endregion
+    }
+}
