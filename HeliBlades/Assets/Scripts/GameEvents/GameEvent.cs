08683c47f02bf9ad8180595f08de07f119469d7c
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-23 02:33:01 +0200

    game event and game event listener scripts

---
+++ HeliBlades/Assets/Scripts/GameEvents/GameEvent.cs
@@ -0,0 +1,68 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-23 02:09:49
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// GameEvent class.
+    /// </summary>
+    [CreateAssetMenu]
+    public class GameEvent : ScriptableObject
+    {
+#region Public serialized variables
+#endregion
+
+
+
+#region Private variables
+        private readonly List<GameEventListener> eventListeners = new List<GameEventListener>();
+#endregion
+
+
+
+#region Public methods and properties
+        [ContextMenu("Raise")]
+        public void Raise()
+        {
+            for(int i = eventListeners.Count -1; i >= 0; i--)
+            {
+                eventListeners[i].OnEventRaised();
+            }
+        }
+
+        public void RegisterListener(GameEventListener listener)
+        {
+            if (!eventListeners.Contains(listener))
+            {
+                eventListeners.Add(listener);
+            }
+        }
+
+        public void UnregisterListener(GameEventListener listener)
+        {
+            if (eventListeners.Contains(listener))
+            {
+                eventListeners.Remove(listener);
+            }
+        }
+#endregion
+
+
+
+#region ScriptableObject methods
+#endregion
+    }
+}
---
+++ HeliBlades/Assets/Scripts/GameEvents/GameEventListener.cs
@@ -0,0 +1,59 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-23 01:58:16
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// GameEventListener class.
+    /// </summary>
+    public class GameEventListener : MonoBehaviour
+    {
+#region Public serialized variables
+        public GameEvent followedEvent;
+        public UnityEvent response;
+#endregion
+
+
+
+#region Private variables
+        private void OnEnable()
+        {
+            followedEvent.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            followedEvent.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            response.Invoke();
+        }
+#endregion
+
+
+
+#region Public methods and properties
+#endregion
+
+
+
+#region Monobehavior methods
+#endregion
+    }
+}
