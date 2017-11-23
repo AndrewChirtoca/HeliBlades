3f3b50e82a3967d69e5478a73159048306f833ba
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-24 01:40:07 +0200

    unity event with vector 3 variable

---
+++ HeliBlades/Assets/Scripts/UnityEvents/UnityEventVec3Var.cs
@@ -0,0 +1,39 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-24 01:19:13
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// UnityEventVec3Var class.
+    /// </summary>
+    [Serializable]
+    public class UnityEventVec3Var : UnityEvent<Vector3Variable>
+    {
+#region Public serialized variables
+#endregion
+
+
+
+#region Private variables
+#endregion
+
+
+
+#region Public methods and properties
+#endregion
+    }
+}
