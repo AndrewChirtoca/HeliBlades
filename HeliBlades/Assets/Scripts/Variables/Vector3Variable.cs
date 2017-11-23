bc8c8d5152c6d5da6e97fa53c636901e4de0bddd
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-23 02:56:43 +0200

    vector3 variables stored in SO script

---
+++ HeliBlades/Assets/Scripts/Variables/Vector3Variable.cs
@@ -0,0 +1,53 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    Insert datetime string (⇧⌘I or Ctrl+Shift+I)
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// Vector3Variable class.
+    /// </summary>
+    [CreateAssetMenu(fileName = "Vector3Variable", menuName = "Variables/Vector3")]
+    public class Vector3Variable : ScriptableObject
+    {
+#region Public serialized variables
+        public Vector3 value;
+#endregion
+
+
+
+#region Private variables
+#endregion
+
+
+
+#region Public methods and properties
+        public void SetValue(Vector3 value)
+        {
+            this.value = value;
+        }
+
+        public void SetValue(Vector3Variable value)
+        {
+            this.value = value.value;
+        }
+#endregion
+
+
+
+#region ScriptableObject methods
+#endregion
+    }
+}
