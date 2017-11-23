c4fd0cded4d6d6ffe3cc8cee1e1d3c8e9b516174
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-24 01:17:24 +0200

    base runtime set implementation

---
+++ HeliBlades/Assets/Scripts/RuntimeSets/RuntimeSet.cs
@@ -0,0 +1,58 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-24 00:55:43
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// RuntimeSet class.
+    /// </summary>
+    public abstract class RuntimeSet<T> : ScriptableObject
+    {
+#region Public serialized variables
+        public List<T> items = new List<T>();
+#endregion
+
+
+
+#region Private variables
+#endregion
+
+
+
+#region Public methods and properties
+        public void Add(T thing)
+        {
+            if (!items.Contains(thing))
+            {
+                items.Add(thing);
+            }
+        }
+
+        public void Remove(T thing)
+        {
+            if (items.Contains(thing))
+            {
+                items.Remove(thing);
+            }
+        }
+#endregion
+
+
+
+#region ScriptableObject methods
+#endregion
+    }
+}
