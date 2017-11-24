9291da7d36f629300c231d622e436ce5551aec6c
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-24 03:31:43 +0200

    simple look at script

---
+++ HeliBlades/Assets/Scripts/LookAt.cs
@@ -0,0 +1,53 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    Insert datetime string (⇧⌘I or Ctrl+Shift+I)
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// LookAt class.
+    /// </summary>
+    public class LookAt : MonoBehaviour
+    {
+#region Public serialized variables
+        public Transform lookTarget;
+        public float turnRate;
+#endregion
+
+
+
+#region Private variables
+#endregion
+
+
+
+#region Public methods and properties
+#endregion
+
+
+
+#region Monobehavior methods
+        public void Update()
+        {
+            if(lookTarget != null)
+            {
+                var lookRot = Quaternion.LookRotation(lookTarget.position - transform.position);
+                float t = turnRate * Time.deltaTime;
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, t);
+            }
+        }
+#endregion
+    }
+}
