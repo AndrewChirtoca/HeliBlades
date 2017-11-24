2279b5a05a0431eba441adc008f8dee124941af8
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-24 02:01:51 +0200

    script defining the targetables runtime set SO

---
+++ HeliBlades/Assets/Scripts/RuntimeSets/TargetablesRuntimeSet.cs
@@ -0,0 +1,44 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-24 01:47:50
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// TargetablesRuntimeSet class.
+    /// </summary>
+    [CreateAssetMenu(fileName = "TargetablesSet", menuName = "Runtime Sets/Targetables")]
+    public class TargetablesRuntimeSet : RuntimeSet<TargetableEntity>
+    {
+#region Public serialized variables
+#endregion
+
+
+
+#region Private variables
+
+#endregion
+
+
+
+#region Public methods and properties
+#endregion
+
+
+
+#region ScriptableObject methods
+#endregion
+    }
+}
