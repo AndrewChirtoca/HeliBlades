3ed6411eac8a3e5fc55c27d6cec53ac771e3242a
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-25 17:11:50 +0200

    extreamly straight forward projectile launcher script first pass

---
+++ HeliBlades/Assets/Scripts/Projectiles/ProjectileLauncher.cs
@@ -0,0 +1,57 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-25 04:17:47
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// ProjectileLauncher class.
+    /// </summary>
+    public class ProjectileLauncher : MonoBehaviour
+    {
+#region Public serialized variables
+        public Projectile projectilePrefab;
+#endregion
+
+
+
+#region Private variables
+#endregion
+
+
+
+#region Public methods and properties
+        public void LaunchProjectile()
+        {
+            var pos = transform.TransformPoint(new Vector3(2.8f, -1.0f, 2.0f));
+            var instance = Instantiate(projectilePrefab, pos, transform.rotation);
+            instance.Launch();
+        }
+#endregion
+
+
+
+#region Monobehavior methods
+#endregion
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                LaunchProjectile();
+            }
+        }
+    }
+}
