0f2bd6a733be7139c72cf9f26e6061fa698e338d
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-26 00:34:26 +0200

    collision enter handler script that allowes to respond to on collosition enter event using the listeners assembled from the inspector

---
+++ HeliBlades/Assets/Scripts/CollisionEnterHandler.cs
@@ -0,0 +1,66 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-26 00:00:55
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// CollisionEnterHandler class.
+    /// </summary>
+    public class CollisionEnterHandler : MonoBehaviour
+    {
+#region Public serialized variables
+        public UnityEvent onCollisionEnterResponce;
+        // public UnityEvent onCollisionExit;
+        // public UnityEvent onTriggerEnter;
+        // public UnityEvent onTriggerExit;
+#endregion
+
+
+
+#region Private variables
+#endregion
+
+
+
+#region Public methods and properties
+#endregion
+
+
+
+#region Monobehavior methods
+        private void OnCollisionEnter(Collision collision)
+        {
+            onCollisionEnterResponce.Invoke();
+        }
+
+        // private void OnCollisionExit(Collision collision)
+        // {
+        //     onCollisionExit.Invoke();
+        // }
+
+        // private void OnTriggerEnter(Collider collider)
+        // {
+        //     onTriggerEnter.Invoke();
+        // }
+
+        // private void OnTriggerExit(Collider collider)
+        // {
+        //     onTriggerExit.Invoke();
+        // }
+#endregion
+    }
+}
