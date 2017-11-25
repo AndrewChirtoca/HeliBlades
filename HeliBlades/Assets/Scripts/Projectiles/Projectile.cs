472b7a899fbeecb4c3ef3fbbcf96d3a1e54db638
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-25 17:06:21 +0200

    first pass on the projectile script and a projectile guide SO

---
+++ HeliBlades/Assets/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,79 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-25 04:56:39
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.Events;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// Projectile class.
+    /// </summary>
+    public class Projectile : MonoBehaviour
+    {
+#region Public serialized variables
+        public GameObjectVariable target;
+        public ProjectileGuide guidence;
+        public UnityEvent onLaunch;
+        public UnityEvent onCollision;
+#endregion
+
+
+
+#region Private variables
+        private Coroutine _guidanceRoutineInst;
+#endregion
+
+
+
+#region Public methods and properties
+        [ContextMenu("Launch")]
+        public void Launch()
+        {
+            if(_guidanceRoutineInst != null)
+            {
+                var msg = "Attempt on repeated projectile launch. Guidance in progress. Aborting.";
+                Debug.LogWarning(msg);
+                return;
+            }
+
+            onLaunch.Invoke();
+            StartGuidance();
+        }
+#endregion
+
+
+
+#region Monobehavior methods
+        private void OnCollisionEnter(Collision collision)
+        {
+            onCollision.Invoke();
+        }
+#endregion
+
+        private void StartGuidance()
+        {
+            _guidanceRoutineInst = StartCoroutine(guidence.GuidanceRoutine(this.gameObject, target));
+        }
+
+        private void StopGuidance()
+        {
+            if(_guidanceRoutineInst != null)
+            {
+                StopCoroutine(_guidanceRoutineInst);
+            }
+        }
+    }
+}
---
+++ HeliBlades/Assets/Scripts/Projectiles/ProjectileGuide.cs
@@ -0,0 +1,49 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-25 15:48:03
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// ProjectileGuide class.
+    /// </summary>
+    public abstract class ProjectileGuide : ScriptableObject
+    {
+#region Public serialized variables
+#endregion
+
+
+
+#region Private variables
+#endregion
+
+
+
+#region Public methods and properties
+#endregion
+
+
+
+#region ScriptableObject methods
+#endregion
+
+
+        public virtual IEnumerator GuidanceRoutine(GameObject projInst, GameObjectVariable target)
+        {
+            yield return null;
+        }
+    }
+}
