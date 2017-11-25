d096c8da8a8b8a96105704afdbc98f34c1672d81
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-25 02:03:40 +0200

    created the follow leader config SO script

---
+++ HeliBlades/Assets/Scripts/FollowLeaderConfig.cs
@@ -0,0 +1,47 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-25 01:49:35
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// FollowLeaderConfig class.
+    /// </summary>
+    [CreateAssetMenu(fileName = "FollowLeaderConfig", menuName = "Configs/Follow Leader")]
+    public class FollowLeaderConfig : ScriptableObject
+    {
+#region Public serialized variables
+        public float moveRate;
+        public Space space;
+        public Vector3 followOffset;
+#endregion
+
+
+
+#region Private variables
+
+#endregion
+
+
+
+#region Public methods and properties
+#endregion
+
+
+
+#region ScriptableObject methods
+#endregion
+    }
+}
