deb6f8ced3a79fdad0fe091a941ea0909928b2c6
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-26 11:43:39 +0200

    structure explosion script & attached to the anti air gun prefab

---
+++ HeliBlades/Assets/Scripts/StructureExplosion.cs
@@ -0,0 +1,49 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-26 11:31:53
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// StructureExplosion class.
+    /// </summary>
+    public class StructureExplosion : MonoBehaviour
+    {
+#region Public serialized variables
+        public GameObject effectPrefab;
+#endregion
+
+
+
+#region Private variables
+
+#endregion
+
+
+
+#region Public methods and properties
+#endregion
+
+
+
+#region Monobehavior methods
+        private void OnCollisionEnter(Collision collision)
+        {
+            var explosionInst = Instantiate(effectPrefab);
+            Destroy(explosionInst, 3.0f);
+            Destroy(gameObject);
+        }
+#endregion
+    }
+}
