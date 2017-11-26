4ba0930773c7a1d53d1a2b80ae76cce7f7ccf802
Andrei Chirtoca <andrewchirtoca@gmail.com>
2017-11-26 12:58:16 +0200

    first pass on anti air gun fire

---
+++ HeliBlades/Assets/Scripts/AntiAirGunFire.cs
@@ -0,0 +1,94 @@
+﻿//===----------------------------------------------------------------------===//
+//
+//  vim: ft=cs tw=80
+//
+//  Date:    2017-11-26 11:51:31
+//  Creator: Chirtoca Andrei <andrewchirtoca@gmail.com>
+//
+//===----------------------------------------------------------------------===//
+
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+
+namespace HeliBlades
+{
+    /// <summary>
+    /// AntiAirGunFire class.
+    /// </summary>
+    public class AntiAirGunFire : MonoBehaviour
+    {
+#region Public serialized variables
+        public GameObject firingEffectPrefab;
+        public float maxProjLifetime = 5.0f;
+        public float fireDelay = 3.0f;
+        public LookAt yAxisRotator;
+        public LookAt xAxisRotator;
+        public Transform launchPod;
+#endregion
+
+
+
+#region Private variables
+        private Coroutine _shootingRoutine;
+#endregion
+
+
+
+#region Public methods and properties
+        public void AimAndShoot(GameObjectVariable target)
+        {
+            Aim(target);
+            StartShooting();
+        }
+
+        public void Aim(GameObjectVariable target)
+        {
+            Aim(target.value.transform);
+        }
+
+        public void Aim(Transform target)
+        {
+            yAxisRotator.SetLookTarget(target);
+            xAxisRotator.SetLookTarget(target);
+        }
+
+        public void StartShooting()
+        {
+            StopShooting();
+            _shootingRoutine = StartCoroutine(ShootingRoutine());
+        }
+
+        public void StopShooting()
+        {
+            if(_shootingRoutine != null)
+            {
+                StopCoroutine(_shootingRoutine);
+            }
+        }
+#endregion
+
+
+
+#region Monobehavior methods
+#endregion
+
+
+        private IEnumerator ShootingRoutine()
+        {
+            while(true)
+            {
+                yield return new WaitForSeconds(fireDelay);
+                //Vector3 pos = launchPod.position;
+                //Quaternion rot = launchPod.rotation;
+                //var bullet = Instantiate(firingEffectPrefab, pos, rot);
+                //Destroy(bullet, maxProjLifetime);
+                gameObject.GetComponent<ProjectileLauncher>().LaunchProjectile();
+            }
+        }
+    }
+}
